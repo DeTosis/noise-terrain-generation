1d89f28a65df14818c5570069c31d8dab0eb94f3
DeTosis <92999941+DeTosis@users.noreply.github.com>
2025-08-11 17:31:12 +0500

    improved noise generation

---
+++ NosieToPBM/src/NoiseGen.h
@@ -0,0 +1,54 @@
+#pragma once
+
+struct NosieSettings
+{
+	float lacunarity;
+	float persistance;
+
+	float noiseScale;
+	float baseFrequency;
+	float octaves;
+
+	float maxHeight;
+	bool  ridge;
+	int	  ridgePower;
+};
+
+class NoiseGenerator
+{
+public:
+	inline float GetHeight(FastNoiseLite& noise, int x, int y, NosieSettings& set)
+	{
+		float frequency = set.baseFrequency;
+		float amplitude;
+		float ampSum = 0.0f;
+
+		float noiseHeight = 0.0f;
+		noise.SetFrequency(frequency);
+		for (int octave = 0; octave < set.octaves; octave++)
+		{
+			frequency = (std::powf(set.lacunarity, octave));
+			amplitude = (std::powf(set.persistance, octave));
+
+			float sampleX = x / set.noiseScale * frequency;
+			float sampleY = y / set.noiseScale * frequency;
+
+			float perlinValue = noise.GetNoise(sampleX, sampleY);
+
+			if (set.ridge)
+			{
+				perlinValue = 1.0 - fabs(perlinValue);
+				perlinValue = pow(perlinValue, set.ridgePower);
+			}
+
+			noiseHeight += perlinValue * amplitude;
+			ampSum += amplitude;
+		}
+		noiseHeight /= ampSum;
+
+		noiseHeight = (noiseHeight + 1.0f) / 2.0f;
+		noiseHeight *= set.maxHeight;
+
+		return noiseHeight;
+	}
+};
--- NosieToPBM/src/Source.cpp
+++ NosieToPBM/src/Source.cpp
@@ -2,58 +2,7 @@
 #include <iostream>
 #include <fstream> 
 # include "fastNoise/FastNoiseLite.h"
-
-float RidgeNoise(FastNoiseLite& noise, float x, float y)
-{
-	return 2 * (0.5f - std::abs((0.5f - noise.GetNoise(x, y))));
-}
-
-struct NoiseSettings
-{
-
-};
-
-
-float GetHeight(FastNoiseLite& noise, int x, int y, int octaves, int maxHeight)
-{
-	float noiseHeight = 0.0f;
-
-	float lacunarity = 2.0f;
-	float persistance = 0.55f;
-
-	float frequency = 0.01f;
-	float amplitude;
-
-	float ampSum = 0.0f;
-	float nearest = 11.0f;
-	float scale = 1.1f;
-
-	float bigScale = 1.5f;
-	float detailScale = 0.25f;
-	float bias = 0.15f;
-
-	noise.SetFrequency(frequency);
-	for (int octave = 0; octave < octaves; octave++)
-	{
-		frequency = (std::powf(lacunarity, octave));
-		amplitude = (std::powf(persistance, octave));
-
-		float sampleX = x / scale * frequency;
-		float sampleY = y / scale * frequency;
-
-		float perlinValue = noise.GetNoise(sampleX, sampleY);
-		noiseHeight += (float)(perlinValue * amplitude);
-
-		ampSum += amplitude;
-	}
-
-	noiseHeight /= ampSum;
-
-	noiseHeight = (noiseHeight + 1.0f) / 2.0f;
-	noiseHeight *= maxHeight;
-
-	return noiseHeight;
-}
+#include "NoiseGen.h"
 
 int main()
 {
@@ -61,33 +10,32 @@ int main()
 	noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
 	noise.SetSeed(1488);
 
+	int h = 512;
+	int w = 512;
+	int maxHeight = 16;
 
-	int h = 400;
-	int w = 400;
-	int maxHeight = 12;
-	
-	std::string header = std::format("P2\n\{} {}\n{}\n",h,w, maxHeight);
+	NosieSettings def{4.0f, 0.55f, 1.1f, 0.01f, 8, maxHeight, true, 3};
+	NosieSettings biom{2.5f, 0.3f, 0.4f, 0.001f, 4, maxHeight, false, 0};
+
+	std::string header = std::format("P2\n\{} {}\n{}\n", h, w, maxHeight);
 	std::ofstream outFile("out.pgm");
 	if (!outFile.is_open())
-	{
-		std::cerr << "[ FILE COULD NOT BE OPENED]" << std::endl;
 		return -1;
-	}
-
+	
 	outFile << header;
 
+	NoiseGenerator noiseGen;
 
 	for (int x = 0; x < h; x++)
 	{
 		for (int y = 0; y < w; y++)
 		{
-			float noiseH = GetHeight(noise, x, y, 8, maxHeight);
+			float noiseH = noiseGen.GetHeight(noise, x, y, biom);
 			outFile << static_cast<int>(noiseH) << " ";
 		}
 		outFile << "\n";
 	}
 	outFile.close();
-
 
 	std::cout << "Application ended" << std::endl;
 }
